656ba146b253ae345cd7d3cd9d4f293c705bc119
Sara Adeldatter Tyldum <98975412+saratyldum@users.noreply.github.com>
2022-10-14 12:45:28 +0200

    Created starting variables in JS

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
-// for slide in slides
-// if visible legg til class, hvis ikke fjern
+const slideshowContainer = document.querySelector('.slideshow__container');
+const slideshowItem = document.querySelector('.slideshow__item');
+const previousButton = document.querySelector('.navigating__button--left');
+const nextButton = document.querySelector('.navigating__button--right')
+const navigationBar = document.querySelector('.nav');
+const navigaitonDots = document.querySelector('.nav__button');
+
