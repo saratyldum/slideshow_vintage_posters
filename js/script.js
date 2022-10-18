5af555bbd0d5a89d40b185cd2012abb8e3bb86bd
saratyldum <98975412+saratyldum@users.noreply.github.com>
2022-10-18 17:48:13 +0200

    Added functions for moving slides

--- js/script.js
+++ js/script.js
@@ -1,30 +1,42 @@
+'use strict'
+
 const container = document.querySelector('.slideshow__container');
-const containerInner = document.querySelector('.slideshow__slider');
-const items = [...containerInner.children];
+const slideshowCarousel = document.querySelector('.slideshow__slider');
+const slideshowItems = [...slideshowCarousel.children];
 const previousButton = document.querySelector('.navigating__button--left');
 const nextButton = document.querySelector('.navigating__button--right')
 const navigationBar = document.querySelector('.nav');
 const navigaitonDots = [...navigationBar.children];
 
-const itemSize = items[0].getBoundingClientRect().width;
+const itemSize = slideshowItems[0].getBoundingClientRect().width;
 
-
-
-nextButton.addEventListener('click', handleNextButtonOnclick);
-previousButton.addEventListener('click', handlePreviousButtonOnclick);
-
-function handleNextButtonOnclick() {
-	container.scrollLeft += 416;
+//setter bildene på rad og rekke
+const setSlidePosition = function (item, index) {
+	item.style.left = itemSize * index + 'px'
 };
 
+slideshowItems.forEach(setSlidePosition);
 
-function handlePreviousButtonOnclick() {
-	container.scrollLeft -= 416;
-};
+//flytter slideshowet
+function moveToSlide(slideshowCarousel, currentSlide, targetSlide) {
+	slideshowCarousel.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+	currentSlide.classList.remove('slideshow__item--active');
+	targetSlide.classList.add('slideshow__item--active');
+}
 
+//høyreknapp
+nextButton.addEventListener('click', event => {
+	const currentSlide = slideshowCarousel.querySelector('.slideshow__item--active');
+	const nextSlide = currentSlide.nextElementSibling;
 
-// Navigating buttons
-// Click left
-//Click right
-//move to nav buttons position
-//Move when clicking tastatur
+	moveToSlide(slideshowCarousel, currentSlide, nextSlide);
+});
+
+//venstreknapp
+previousButton.addEventListener('click', event => {
+	const currentSlide = slideshowCarousel.querySelector('.slideshow__item--active');
+	const previousSlide = currentSlide.previousElementSibling;
+
+	moveToSlide(slideshowCarousel, currentSlide, previousSlide);
+});
+
